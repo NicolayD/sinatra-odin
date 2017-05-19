47a13fb939463548519d4181dd7ed088a7bc99f0
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-05-19 14:57:48 +0300

    Add working caesar cipher and display it

--- caesar_sinatra.rb
+++ caesar_sinatra.rb
@@ -3,5 +3,10 @@ require 'sinatra/reloader' if development?
 require_relative 'lib/caesar_cipher'
 
 get '/' do
-	erb :index
+	if params['phrase'] && params['factor']
+		phrase = params['phrase']
+		factor = params['factor']
+		output = caesar_cipher(phrase,factor)
+	end
+	erb :index, locals: { output: output }
 end
