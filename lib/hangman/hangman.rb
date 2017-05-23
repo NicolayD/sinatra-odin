a245af6486c6d070fb64c724908e46145b284518
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-05-23 15:12:27 +0300

    Refactor hangman initialize

--- lib/hangman/hangman.rb
+++ lib/hangman/hangman.rb
@@ -3,7 +3,10 @@ class Hangman
 
 	def initialize
 		dictionary = File.read("lib/hangman/5desk.txt").split
-		@answer = dictionary[rand(dictionary.length)] while @answer.length < 5 || @answer.length > 12
+		@answer = dictionary[rand(dictionary.length)] 
+		while @answer.length < 5 || @answer.length > 12
+			@answer = dictionary[rand(dictionary.length)]
+		end
 		@hidden_answer = []
 		@answer.length.times { @hidden_answer << "_" }
 		@turns = 12
@@ -28,9 +31,7 @@ class Hangman
 			@incorrect << letter
 		end
 		@message = "#{@turns} turns left."
-
 		@message = "You lose! The answer is #{@answer}." if @turns == 0
-
-		@message = "You won!" if @guess.join = @answer
+		@message = "You won!" if @guess.join == @answer
 	end
 end
