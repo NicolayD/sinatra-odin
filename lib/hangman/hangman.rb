b9d58a5de067514607d102ac33624d550fe48029
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2017-06-01 22:41:28 +0300

    Refactor hangman backend

--- lib/hangman/hangman.rb
+++ lib/hangman/hangman.rb
@@ -18,12 +18,12 @@ class Hangman
 
 	def check_guess letter
 		answer_array = @answer.split(//)
-		if answer_array.include? letter || answer_array.include?(letter.upcase)			
+		if answer_array.include?(letter.downcase) || answer_array.include?(letter.upcase)			
 			answer_array.each_with_index do |let, index|		
-				if letter == let || letter.upcase == let
-					letter == let ? @guess[index] = let : @guess[index] = let.upcase
+				if letter.downcase == let || letter.upcase == let
+					letter == let.upcase ? @guess[index] = let : @guess[index] = let.downcase
 					@correct << letter.downcase if !@correct.include?(letter.downcase)
-					letter == let ? @hidden_answer[index] = let : @hidden_answer[index] = let.upcase
+					letter == let.upcase ? @hidden_answer[index] = let : @hidden_answer[index] = let.downcase
 				end
 			end
 		else
